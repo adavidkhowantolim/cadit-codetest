1dd60296b2d04119061fdd2898719602f32f42b8
David Khowanto <44863712+adavidkhowantolim@users.noreply.github.com>
2021-07-03 18:48:37 +0700

    add comments

--- problem2.py
+++ problem2.py
@@ -1,6 +1,9 @@
 # Problem 2: Sensor Aggregation
 # David Khowanto
+
 # This program didn't check for acceptable file type and content of the file
+# Assumption: - file type and its contents are always correct 
+#             - only aggregation ('min','max','median','mean') are accepted
 
 ## Import libraries
 import json
@@ -8,10 +11,10 @@ from pandas import DataFrame, pivot_table
 
 ## Aggregation Dataframe Function
 def aggregateBy(df, aggfunc):
-  if (aggfunc == 'all'): 
-    aggfunc = ['min','max','median','mean']
   if (aggfunc != 'all' and aggfunc != 'min' and aggfunc != 'max' and aggfunc != 'median' and aggfunc != 'mean'):
     return -1
+  if (aggfunc == 'all'): 
+    aggfunc = ['min','max','median','mean']
   return pivot_table(df, values=['humidity','temperature'], index='timestamp', columns='roomArea',aggfunc=aggfunc)
 
 ## Main Logic for Sensor Aggregation
@@ -27,13 +30,18 @@ def sensorAggregation(json_file, aggfunc):
   # Aggregate humidity and temperature on roomArea and by day (timestamp)
   result_df = aggregateBy(sensor_df, aggfunc)
   if(isinstance(result_df, DataFrame)):
+    print("Aggregation: ", ('min','max','median','mean') if (aggfunc=='all') else aggfunc)
+    print(result_df)
     return(result_df, aggfunc)
   else:
-    return(aggfunc, "Aggregation not Supported")
+    print("Aggregation", aggfunc, "Not Supported")
+    return(-1, aggfunc)
 
-sensorAggregation('JSON Files/sensor_data.json', 'all')
-sensorAggregation('JSON Files/sensor_data.json', 'min')
-sensorAggregation('JSON Files/sensor_data.json', 'max')
-sensorAggregation('JSON Files/sensor_data.json', 'median')
-sensorAggregation('JSON Files/sensor_data.json', 'mean')
-sensorAggregation('JSON Files/sensor_data.json', 'avg')
+## Run the program
+# sensorAggregation('JSON Files/sensor_data.json', 'all')
+# sensorAggregation('JSON Files/sensor_data.json', 'min')
+# sensorAggregation('JSON Files/sensor_data.json', 'max')
+# sensorAggregation('JSON Files/sensor_data.json', 'median')
+# sensorAggregation('JSON Files/sensor_data.json', 'mean')
+# sensorAggregation('JSON Files/sensor_data.json', 'avg')
+# sensorAggregation('JSON Files/sensor_data.json', "percentile")
